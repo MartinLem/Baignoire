535d2e048eb8587ff3d4d3fe9624e79739333351
MartinLem <martinlemaitre@hotmail.fr>
2019-02-10 14:01:11 +0100

    ajout appli.java

---
+++ src/main/java/fr/ul/miage/main/Appli.java
@@ -0,0 +1,16 @@
+package fr.ul.miage.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Appli extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
